9f15e2d9c4ccf15826979776ae63f3d22c6f241c
mnaufalammarr <43328295+mnaufalammarr@users.noreply.github.com>
2022-06-22 23:10:09 +0700

    Add files via upload

---
+++ build/precache-manifest.13a3b32ae12f781d4c2e31938546d61c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1223c76046f355b5b5b109d4c0487e28",
+    "url": "/ToDoList-JS-FGA/index.html"
+  },
+  {
+    "revision": "e725997b81f792b0e5d8",
+    "url": "/ToDoList-JS-FGA/static/css/2.7b4682b0.chunk.css"
+  },
+  {
+    "revision": "f584d9d69afab0315cad",
+    "url": "/ToDoList-JS-FGA/static/css/main.777ea05e.chunk.css"
+  },
+  {
+    "revision": "e725997b81f792b0e5d8",
+    "url": "/ToDoList-JS-FGA/static/js/2.c9fe867a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ToDoList-JS-FGA/static/js/2.c9fe867a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f584d9d69afab0315cad",
+    "url": "/ToDoList-JS-FGA/static/js/main.67617cbf.chunk.js"
+  },
+  {
+    "revision": "3674afe7c94ee839e1ff",
+    "url": "/ToDoList-JS-FGA/static/js/runtime-main.bbab5d92.js"
+  }
+]);
